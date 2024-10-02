3800610020c06e8c11575dd8e62cf5d6c49ae73f
Rohith Gowtham G <rohithgowthamg4927@gmail.com>
2024-10-02 19:51:06 +0530

    First Commit of Cloud Resume Challenge

--- script.js
+++ script.js
@@ -17,18 +17,31 @@ window.dispatchEvent(new Event('resize'));
 document.addEventListener('DOMContentLoaded', () => {
     const hamburger = document.getElementById('hamburger');
     const sidebar = document.getElementById('sidebar');
-    
+
+    function adjustSidebarHeight() {
+        if (window.innerWidth <= 768) {
+            const headerHeight = document.querySelector('.sidebar-header').offsetHeight;
+            const socialHeight = document.querySelector('.socials').offsetHeight;
+
+            const availableHeight = window.innerHeight - headerHeight - socialHeight;
+            sidebar.style.height = `${availableHeight}px`;
+        } else {
+            sidebar.style.height = '100vh';
+        }
+    }
+
     hamburger.addEventListener('click', () => {
-        console.log('hamburger clicked');
         sidebar.classList.toggle('active');
+        adjustSidebarHeight();
     });
+
+    window.addEventListener('resize', adjustSidebarHeight);
+    adjustSidebarHeight();
 });
 
 const count = document.querySelector(".page-views");
 async function updateViewerCount() {
-    let resp = await fetch(
-        "https://vdozaflmfqf7t5bs5ez2lthoqm0qkivc.lambda-url.ap-south-1.on.aws/"
-    );
+    let resp = await fetch("https://vdozaflmfqf7t5bs5ez2lthoqm0qkivc.lambda-url.ap-south-1.on.aws/");
     let views = await resp.json();
     count.innerHTML = `Total Views👀: ${views}`;
 }
